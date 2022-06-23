9df58feffd89ac1fd6c7d74d1d5e834099dbb75c
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-06-23 15:12:20 +0200

    builder

---
+++ src/main/java/builder/lombok/LombokBuilderUsage.java
@@ -0,0 +1,14 @@
+package builder.lombok;
+
+public class LombokBuilderUsage {
+    public static void main(String[] args) {
+        GraphicsCard graphicsCard = GraphicsCard.builder()
+                .memoryInMb(2048)
+                .modelName("GF1660")
+                .producer("Asus")
+                .series("1xxx")
+                .build();
+
+        System.out.println(graphicsCard);
+    }
+}
---
+++ src/main/java/builder/toy/ToyBuilderUsage.java
@@ -0,0 +1,14 @@
+package builder.toy;
+
+public class ToyBuilderUsage {
+    public static void main(String[] args) {
+        final Toy toy = new ToyBuilder()
+                .withMadeOf("plastic")
+                .withName("Resorak")
+                .withType("Small car")
+                .build();
+
+        System.out.println(toy);
+    }
+
+}
