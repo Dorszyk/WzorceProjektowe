ba63ea299de706fc6853ab4689531e87ee173809
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-06-23 14:42:06 +0200

    singleton

---
+++ src/main/java/singleton/singletoneagerclass/SimpleCounter.java
@@ -0,0 +1,26 @@
+package singleton.singletoneagerclass;
+
+public class SimpleCounter {
+
+    // pole statyczne, w którym przetrzymujemy referencję singletonu
+    // jest to singleton typu eager więc instancję tworzymy od razu, przypisując ją do pola
+    private static final SimpleCounter INSTANCE = new SimpleCounter();
+
+    // getter dla referencji singletonu
+    public static SimpleCounter getInstance() {
+        return INSTANCE;
+    }
+
+    // ukryty konstruktor
+    private SimpleCounter() {}
+
+    private int currentCount = 0;
+
+    public int getCurrentCount() {
+        return currentCount;
+    }
+
+    public void increment() {
+        currentCount++;
+    }
+}
---
+++ src/main/java/singleton/singletoneagerclass/SimpleCounterUsage.java
@@ -0,0 +1,13 @@
+package singleton.singletoneagerclass;
+
+public class SimpleCounterUsage {
+    public static void main(String[] args) {
+        SimpleCounter simpleCounterA = SimpleCounter.getInstance();
+        SimpleCounter simpleCounterB = SimpleCounter.getInstance();
+        System.out.println(simpleCounterA == simpleCounterB); // true -> obie referencje wskazują na ten SAM obiekt
+
+        simpleCounterA.increment();
+        simpleCounterB.increment();
+        System.out.println(simpleCounterA.getCurrentCount()); // 2
+    }
+}
